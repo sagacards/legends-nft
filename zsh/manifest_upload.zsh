dc762c7e0c2e5fcad423ca20e3ce0d4b20b2d208
Jorgen <jorgen@buildnode.io>
2022-01-24 16:46:51 -0800

    double check mainnet manifest uploads

--- zsh/manifest_upload.zsh
+++ zsh/manifest_upload.zsh
@@ -2,6 +2,18 @@
 canister=${1:-legends-test}
 network=${2:-local}
 
+# Confirm before deploying to mainnet
+if [[ $network != "local" ]]
+then
+    echo "Confirm mainnet uploads"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) break;;
+            No ) exit;;
+        esac
+    done
+fi
+
 manifest="./config/manifests/$canister.csv"
 [ ! -f $manifest ] && { echo "$manifest file not found"; exit 99; }
 
